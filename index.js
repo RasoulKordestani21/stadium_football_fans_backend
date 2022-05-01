49bb3e0869eb0acd2c44148b4537d86e1a31819c
Rasoul <rasoulkordestani12@gmail.com>
2022-05-01 09:29:19 +0430

    fix: update header of /course request

--- index.js
+++ index.js
@@ -10,36 +10,38 @@ const courses = [
   { id: 3, name: "Human Computer Interaction" }
 ];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   //when we get an http get request to the root/homepage
   res.send("Hello World");
 });
 
 //when we route to /courses
-app.get("/courses", function(req, res) {
+app.get("/courses", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.send(courses); //respond with the array of courses
 });
 
 //To get a specific course, we need to define a parameter id
-app.get("/courses/:id", function(req, res) {
+app.get("/courses/:id", function (req, res) {
   const course = courses.find(c => c.id === parseInt(req.params.id));
 
   //if the course does not exist return status 404 (not found)
   if (!course)
-      return res
-          .status(404)
-          .send("The course with the given id was not found");
+    return res.status(404).send("The course with the given id was not found");
 
   //return the object
   res.send(course);
 });
 
 //using the http post request we can create a new course
-app.post("/courses", function(req, res) {
+app.post("/courses", function (req, res) {
   //create a course object
   const course = {
-      id: courses.length + 1,
-      name: req.body.name
+    id: courses.length + 1,
+    name: req.body.name
   };
 
   //add the course to the array
@@ -49,14 +51,12 @@ app.post("/courses", function(req, res) {
   res.send(course);
 });
 
-app.put("/courses/:id", function(req, res) {
+app.put("/courses/:id", function (req, res) {
   //get the course
   const course = courses.find(c => c.id === parseInt(req.params.id));
 
   if (!course)
-      return res
-          .status(404)
-          .send("The course with the given id was not found");
+    return res.status(404).send("The course with the given id was not found");
 
   //update the course
   course.name = req.body.name;
@@ -65,14 +65,12 @@ app.put("/courses/:id", function(req, res) {
   res.send(course);
 });
 
-app.put("/courses/:id", function(req, res) {
+app.put("/courses/:id", function (req, res) {
   //get the course
   const course = courses.find(c => c.id === parseInt(req.params.id));
 
   if (!course)
-      return res
-          .status(404)
-          .send("The course with the given id was not found");
+    return res.status(404).send("The course with the given id was not found");
 
   //update the course
   course.name = req.body.name;
@@ -81,6 +79,6 @@ app.put("/courses/:id", function(req, res) {
   res.send(course);
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`Listening on Port ${PORT}`);
 });
