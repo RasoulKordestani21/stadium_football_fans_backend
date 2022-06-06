39324e604594eaf6b8a7c8b03a25bd63511f11cc
Rasoul <rasoulkordestani12@gmail.com>
2022-06-07 02:52:00 +0430

    Revert "fix: comment header of request"
    This reverts commit b51d8aa441e455273f7e298f72587b0fdef781de.

--- app.js
+++ app.js
@@ -1,41 +1,119 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const PORT = process.env.PORT || 5000;
+const express = require("express");
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//import image
+const image = require("./dataBase");
 
-var app = express();
+// for (let i = 18; i <= 26; i++) {
+//   console.log(image[Math.floor(i / image.length)][i % image.length]);
+// }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const maskPageDefiner = chairNum => {
+  return image[Math.floor(+chairNum / image.length)][+chairNum % image.length];
+};
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const courses = [
+  { id: 1, name: "Algorithms" },
+  { id: 2, name: "Software Engineering" },
+  { id: 3, name: "Human Computer Interaction" }
+];
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", function (req, res) {
+  //handling cors error
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // res.setHeader("salam", "aaaaaa");
+
+  //when we get an http get request to the root/homepage
+  let maskPage, error;
+  if (
+    req.query.chairNum > -1 &&
+    req.query.chairNum < image.length * image[0].length
+  ) {
+    maskPage = maskPageDefiner(req.query.chairNum);
+    error = "";
+  } else {
+    maskPage = -1;
+    error = "لطفا عدد ورودی را درست وارد نمایید.";
+  }
+  res.send({ maskPage, error });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//when we route to /courses
+app.get("/courses", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.send(courses); //respond with the array of courses
 });
 
-module.exports = app;
+app.get("/salam", (req, res) => {
+  console.log(req,"this is req")
+  res.send({ name: "ali" });
+});
+
+//To get a specific course, we need to define a parameter id
+app.get("/courses/:id", function (req, res) {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  //if the course does not exist return status 404 (not found)
+  if (!course)
+    return res.status(404).send("The course with the given id was not found");
+
+  //return the object
+  res.send(course);
+});
+
+//using the http post request we can create a new course
+app.post("/courses", function (req, res) {
+  //create a course object
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+
+  //add the course to the array
+  courses.push(course);
+
+  //return the course
+  res.send(course);
+});
+
+app.put("/courses/:id", function (req, res) {
+  //get the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course)
+    return res.status(404).send("The course with the given id was not found");
+
+  //update the course
+  course.name = req.body.name;
+
+  //return the updated object
+  res.send(course);
+});
+
+app.put("/courses/:id", function (req, res) {
+  //get the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course)
+    return res.status(404).send("The course with the given id was not found");
+
+  //update the course
+  course.name = req.body.name;
+
+  //returns the updated object
+  res.send(course);
+});
+
+app.listen(PORT, function () {
+  console.log(process.env.PORT);
+  console.log(`Listening on Port ${PORT}`);
+});
