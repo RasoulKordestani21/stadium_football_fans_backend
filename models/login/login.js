b86af514cc795bd6216ae3f9d7834e880f756b9c
Rasoul <rasoulkordestani12@gmail.com>
2022-08-28 23:52:31 +0430

    feat: add logins requests

---
+++ models/login/login.js
@@ -0,0 +1,89 @@
+const express = require("express");
+// const router = require("../../routes");
+const loginRouter = express.Router();
+const mongoose = require("mongoose");
+
+const loginSchema = mongoose.Schema({
+  username: String,
+  password: String
+});
+
+// const SigninModel = mongoose.model("signin", signinSchema);
+
+const LoginModel = mongoose.model("login", loginSchema);
+loginRouter.get("/signin", async (req, res) => {
+  //   console.log(req.path);
+  const findUser = await LoginModel.findOne({
+    username: req.query.username,
+    password: req.query.password
+  });
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  // res.setHeader("Access-Control-Allow-Credentials", true);
+  // console.log(posts);
+  if (findUser) {
+    res.send({ isSuccess: true });
+  } else {
+    res.send({
+      error: "not found",
+      message: "نام کاربری یا پسورد اشتباه است."
+    });
+  }
+});
+
+loginRouter.post("/signup", async (req, res) => {
+  //   console.log(req.path);
+  const setUser = new LoginModel({
+    username: req.query.username,
+    password: req.query.password
+  });
+  await setUser
+    .save()
+    .then(() => {
+      res.send({ isSucces: true, message: "ثبت نام با موفقیت انجام شد." });
+    })
+    .catch(() => {
+      res.send({ isSucces: false, message: "عملیات ثبت نام با خطا مواجه شد." });
+    });
+});
+
+// const SignupModel = mongoose.model("signup", signupSchema);
+
+// loginRouter.post("/signup", async (req, res) => {
+//   //   console.log(req.path);
+//   const signup = new SignupModel({
+//     username: req.query.username,
+//     password: req.query.password
+//   });
+//   signup
+//     .save()
+//     .then(() => {
+//       res.setHeader("Access-Control-Allow-Origin", "*");
+//       res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//       res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//       res.setHeader("Access-Control-Allow-Credentials", true);
+//       res.send({ message: "service added one " });
+//     })
+//     .catch(() => res.send({ message: "operation with error" }));
+//   //   const posts = await SignupModel.save({
+//   //     username: req.query.username,
+//   //     password: req.query.password
+//   //   });
+//   //   res.setHeader("Access-Control-Allow-Origin", "*");
+//   //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   //   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   //   res.setHeader("Access-Control-Allow-Credentials", true);
+//   //   if (posts) {
+//   //     res.send(posts);
+//   //   } else {
+//   //     res.send({
+//   //       error: "not found",
+//   //       message: "نام کاربری یا پسورد اشتباه است."
+//   //     });
+//   //   }
+// });
+
+module.exports = loginRouter;
+
+[]
--- routes.js
+++ routes.js
@@ -1,39 +1,22 @@
 const express = require("express");
-const Post = require("./models/Post"); // new
+// const Post = require("./models/Post"); // new
+const loginRouter = require("./models/login/login");
 const router = express.Router();
 
-// Get all posts
-router.get("/posts", async (req, res) => {
-  const posts = await Post.findOne({
-    username: req.query.username,
-    password: req.query.password
-  });
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  if (posts) {
-    res.send(posts);
-  } else {
-    res.send({
-      error: "not found",
-      message: "نام کاربری یا پسورد اشتباه است."
-    });
-  }
-});
+router.use("/", loginRouter);
 
-router.post("/posts", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  console.log(req.query);
-  const post = new Post({
-    username: req.query.username,
-    password: req.query.password
-  });
-  await post.save();
-  res.send(post);
-});
+// router.post("/posts", async (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   console.log(req.query);
+//   const post = new Post({
+//     username: req.query.username,
+//     password: req.query.password
+//   });
+//   await post.save();
+//   res.send(post);
+// });
 
 module.exports = router;
