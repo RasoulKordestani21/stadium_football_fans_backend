b8e246b674abd611d0ad84af46be85a8e1b33f0d
Rasoul <rasoulkordestani12@gmail.com>
2022-08-29 02:35:13 +0430

    fix: add cors package to project and use it

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose"); // new
 const routes = require("./routes");
 require("dotenv").config();
+var cors = require("cors");
+
 // Connect to MongoDB database
 
 mongoose
   .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     const app = express();
+    app.use(cors());
     app.use("/api", routes); // new
 
     app.listen(port || 5000, () => {
